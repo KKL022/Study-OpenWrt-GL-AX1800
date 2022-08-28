47631ae1428cddcf37aaa428bfa91bc32304c372
KKL022 <108229857+KKL022@users.noreply.github.com>
2022-08-28 23:23:10 +0800

    Update packages.js

--- scripts/packages.js
+++ scripts/packages.js
@@ -9,14 +9,14 @@ module.exports = [
     name: 'luci-theme-argon',
     desc: 'Argon主题',
   },
-  {
-    name: 'luci-app-passwall2',
-    desc: 'Passwall2代理软件',
-  },
-  {
-    name: 'luci-app-ssr-plus',
-    desc: 'SSR-Plus 代理软件',
-  },
+  // {
+  //  name: 'luci-app-passwall2',
+  //  desc: 'Passwall2代理软件',
+  // },
+  // {
+  //  name: 'luci-app-ssr-plus',
+  //  desc: 'SSR-Plus 代理软件',
+  // },
   {
     name: 'luci-app-openclash',
     desc: 'Clash的图形代理软件',
@@ -29,10 +29,10 @@ module.exports = [
     name: 'luci-app-diskman',
     desc: '磁盘管理工具',
   },
-  {
-    name: 'luci-app-aria2',
-    desc: 'Aria2下载工具',
-  },
+  // {
+  //  name: 'luci-app-aria2',
+  //  desc: 'Aria2下载工具',
+  // },
   {
     name: 'luci-app-ddns',
     desc: '动态域名 DDNS',
@@ -41,18 +41,18 @@ module.exports = [
     name: 'luci-app-zerotier',
     desc: 'ZeroTier内网穿透',
   },
-  {
-   name: 'luci-app-smartdns',
-   desc: 'Smartdns DNS防污染',
-  },
+  // {
+  // name: 'luci-app-smartdns',
+  // desc: 'Smartdns DNS防污染',
+  // },
   {
     name: 'luci-app-turboacc',
     desc: 'Turbo ACC 网络加速(支持 Fast Path 或者 硬件 NAT)',
   },
-  {
-    name: 'luci-app-cpufreq',
-    desc: 'CPU 性能优化调节',
-  },
+  // {
+  //  name: 'luci-app-cpufreq',
+  //  desc: 'CPU 性能优化调节',
+  // },
   {
     name: 'luci-app-samba4',
     desc: 'Samba4文件共享',
@@ -61,10 +61,10 @@ module.exports = [
    name: 'luci-app-ttyd',
    desc: 'TTYD 终端',
   },
-  {
-    name: 'luci-app-unblockneteasemusic-go',
-    desc: '解网易云歌曲-GO',
-  },
+  // {
+  //  name: 'luci-app-unblockneteasemusic-go',
+  //  desc: '解网易云歌曲-GO',
+  // },
   // {
   //   name: 'luci-app-unblockmusic',
   //   desc: '解网易云歌曲3合1',
