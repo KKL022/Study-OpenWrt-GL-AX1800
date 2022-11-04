295a0684343eaaa76391439c13bdb8632eda67e0
KKL022 <108229857+KKL022@users.noreply.github.com>
2022-11-04 14:32:32 +0800

    Update feeds.js

--- scripts/feeds.js
+++ scripts/feeds.js
@@ -7,48 +7,18 @@
 
 module.exports = [
   {
-    name: 'xiaorouji_package_luci',
-    uri: 'https://github.com/xiaorouji/openwrt-passwall.git',
-    branch: 'luci',
-  },
-  {
-    name: 'xiaorouji_package',
-    uri: 'https://github.com/xiaorouji/openwrt-passwall.git',
-    branch: 'packages',
-  },
-  {
-    name: 'kenzok8_package',
-    uri: 'https://github.com/kenzok8/openwrt-packages.git',
-    branch: 'master',
-  },
-  {
-    name: 'kenzok8_small',
-    uri: 'https://github.com/kenzok8/small.git',
-    branch: 'master',
-  },
+    name: 'NueXini_Packages',
+    uri: 'https://github.com/NueXini/NueXini_Packages.git',
+    branch: 'main',
+  },  
   {
     name: 'immortalwrt_luci',
     uri: 'https://github.com/immortalwrt/luci.git',
     branch: 'openwrt-21.02',
   },
   {
-    name: 'NueXini_Packages',
-    uri: 'https://github.com/NueXini/NueXini_Packages.git',
-    branch: 'main',
-  },
-  // {
-  //   name: 'KKL022_Packages',
-  //   uri: 'https://github.com/KKL022/luci-app-baidupcs-web.git',
-  //   branch: 'master',
-  // },  
-  // {
-  //   name: 'lienol_packages',
-  //   uri: 'https://github.com/Lienol/openwrt-package.git',
-  //   branch: 'main',
-  // },
-  // {
-  //   name: 'open_app_filter',
-  //   uri: 'https://github.com/destan19/OpenAppFilter.git',
-  //   branch: 'oaf-5.1.0',
-  // },
+    name: 'kenzok8_small',
+    uri: 'https://github.com/kenzok8/small.git',
+    branch: 'master',
+  },  
 ];
